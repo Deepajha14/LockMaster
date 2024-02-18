72c80fb55bc6441f23cfde59295ad0038ad91677
Deepa Jha <deepadj1415@gmail.com>
2024-02-18 22:15:12 +0530

    connect db

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "lockmaster"
+export const DB_NAME = "LockMaster"
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,50 @@
-console.log("Hello");
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+import {app} from './app.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then ( () => {
+
+    app.on("error", (error) => {
+        console.log("ERR: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`App is listening on port: ${process.env.PORT}`);
+    })
+3 
+})
+.catch ((error) => {
+
+    console.log("MONGO db connection failed!!! ", error);
+
+})
+
+
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+// import express from "express";
+
+// const app = express()
+
+// ;(  async () => {
+//         try {
+//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//             app.on("error", (error) => {
+//                 console.log("ERRR: ", error);
+//                 throw error
+//             })
+//             app.listen(process.env.PORT, () => {
+//                 console.log(`App is listening on port ${process.env.PORT}`);
+//             })
+//         } catch (error) {
+//             console.log("ERROR: ", error)
+//             throw error
+//         }
+// })()
